fb5e8e16d8cb51d4c36b41492bcd18ca5d8fde1d
Roach <113860476+labroach22@users.noreply.github.com>
2023-04-05 21:52:35 -0700

    fix

--- src/kksubs/data.py
+++ src/kksubs/data.py
@@ -59,6 +59,8 @@ class BaseData(ABC):
     def __repr__(self) -> str:
         return str(self.__dict__)
     def __eq__(self, __value: object) -> bool:
+        if __value is None:
+            return False
         return self.__dict__ == __value.__dict__
     
     @abstractclassmethod
